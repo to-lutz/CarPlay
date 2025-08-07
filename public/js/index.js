e6a9309c9965c54b5e0f114914d7167aa59b2b46
Tobias Lutz <postfach@tolutz.de>
2025-08-07 17:42:35 +0200

    App icon click animation

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    document.getElementById('time-sidebar').textContent = `${hours}:${minutes}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000); // Update every second
+
+const appicons = document.querySelectorAll('.app-icon');
+appicons.forEach((el) => {
+    el.addEventListener('click', () => {
+        el.classList.add('clicked');
+
+        setTimeout(() => {
+            el.classList.remove('clicked');
+        }, 300);
+    });
+});
