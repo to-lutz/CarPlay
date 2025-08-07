943916fe69579416f9cb169f16b807e8a272846f
Tobias Lutz <postfach@tolutz.de>
2025-08-07 16:06:30 +0200

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/html/index.html'));
+});
+
+module.exports = router;
